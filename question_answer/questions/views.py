e91674cabae34f8413eda7aa2ab70a98c6852bb9
Nishant4302 <mrgolani2002@gmail.com>
2023-09-27 10:41:47 +0530

    h

---
+++ question_answer/questions/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def question_answer(request):
+    return render(request , "index.html" )
+
+class QnA():
+    def Q():
+        pass
+    
